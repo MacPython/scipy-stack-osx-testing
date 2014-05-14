6c01afb522a30211c167c0a3b689646b4f2be578
Matthew Brett <matthew.brett@gmail.com>
2014-05-13 23:32:33 -0700

    BF: chain return codes, fix executable prefix

--- test.sh
+++ test.sh
@@ -4,28 +4,47 @@ which $PYTHON
 echo "pip $PIP"
 which $PIP
 
+# Return code
+RET=0
+
 echo "sanity checks"
 $PYTHON -c "import sys; print('\n'.join(sys.path))"
+if [ $? -ne 0 ] ; then RET=1; fi
+RET=`expr $RET + $?`
 for pkg in numpy scipy matplotlib IPython pandas sympy nose
 do
     $PYTHON -c "import ${pkg}; print(${pkg}.__version__, ${pkg}.__file__)"
+    if [ $? -ne 0 ] ; then RET=1; fi
 done
 
 echo "unit tests"
 for pkg in numpy scipy
 do
     $PYTHON -c "import sys; import ${pkg}; sys.exit(not ${pkg}.test().wasSuccessful())"
+    if [ $? -ne 0 ] ; then RET=1; fi
 done
+
 $PYTHON -c "import sys; import matplotlib; sys.exit(not matplotlib.test())"
-PYPREFIX=`$PYTHON -c 'import sys; print(sys.prefix)'`
+if [ $? -ne 0 ] ; then RET=1; fi
+
 if [ -z "$VENV" -a "${TEST}" != "macpython" ]; then
     BIN_PREFIX=''
 else # pip always installs next to Python in virtualenvs, and for macpython
-    PYPREFIX=`$PYTHON -c 'import sys; print(sys.prefix)'`
+    PY_PREFIX=`$PYTHON -c 'import sys; print(sys.prefix)'`
     BIN_PREFIX="${PY_PREFIX}/bin/"
 fi
+
 ${BIN_PREFIX}iptest
+if [ $? -ne 0 ] ; then RET=1; fi
+
 ${BIN_PREFIX}nosetests pandas -e test_fred_multi -e test_fred_parts
+if [ $? -ne 0 ] ; then RET=1; fi
+
 $PYTHON -c "import sys; import sympy; sys.exit(not sympy.test('/basic', '/util'))"
+if [ $? -ne 0 ] ; then RET=1; fi
 
 echo "done testing scipy stack"
+
+# Set the return code
+(exit $RET)
+

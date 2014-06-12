cfd476c315420836cca4d386b205a287de84387f
Matthew Brett <matthew.brett@gmail.com>
2014-06-12 15:07:53 +0100

    Add i386 tests to grid

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ which $PYTHON
 echo "pip $PIP"
 which $PIP
 
+PYTHON_TEST="$ARCH $PYTHON"
+
 # Return code
 RET=0
 
@@ -20,20 +22,20 @@ done
 echo "unit tests"
 for pkg in numpy scipy
 do
-    $PYTHON -c "import sys; import ${pkg}; sys.exit(not ${pkg}.test().wasSuccessful())"
+    $PYTHON_TEST -c "import sys; import ${pkg}; sys.exit(not ${pkg}.test().wasSuccessful())"
     if [ $? -ne 0 ] ; then RET=1; fi
 done
 
 # Matplotlib testing
 # Miss out known fails for Python 3.4
 # https://github.com/matplotlib/matplotlib/pull/2981
-$PYTHON ../mpl_tests.py -e test_override_builtins
+$PYTHON_TEST ../mpl_tests.py -e test_override_builtins
 if [ $? -ne 0 ] ; then RET=1; fi
 
 if [ -z "$VENV" -a "${TEST}" != "macpython" ]; then
     BIN_PREFIX=''
 else # pip always installs next to Python in virtualenvs, and for macpython
-    PY_PREFIX=`$PYTHON -c 'import sys; print(sys.prefix)'`
+    PY_PREFIX=`$PYTHON_TEST -c 'import sys; print(sys.prefix)'`
     BIN_PREFIX="${PY_PREFIX}/bin/"
 fi
 
@@ -50,7 +52,7 @@ ${BIN_PREFIX}nosetests pandas -A "not slow" \
     -e test_dict_complex -e test_dict_numpy_complex
 if [ $? -ne 0 ] ; then RET=1; fi
 
-$PYTHON -c "import sys; import sympy; sys.exit(not sympy.test('/basic', '/util'))"
+$PYTHON_TEST -c "import sys; import sympy; sys.exit(not sympy.test('/basic', '/util'))"
 if [ $? -ne 0 ] ; then RET=1; fi
 
 echo "done testing scipy stack"
